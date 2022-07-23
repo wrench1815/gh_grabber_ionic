e37db2c88e838459ffdb871de917b7e740ef170d
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-07-23 15:45:01 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gh_grabber_android',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;

b7a80ba63995e9da947d6d074666988ad8a1ae77
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-07-23 15:53:36 +0530

    chore: use js instead of ts

---
+++ capacitor.config.js
@@ -0,0 +1,8 @@
+const config = {
+  appId: 'io.ionic.starter',
+  appName: 'gh grabber',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
--- src/views/FolderPage.vue
+++ src/views/FolderPage.vue
@@ -8,27 +8,42 @@
         <ion-title>{{ $route.params.id }}</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">{{ $route.params.id }}</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+
       <div id="container">
         <strong class="capitalize">{{ $route.params.id }}</strong>
-        <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <p>
+          Explore
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://ionicframework.com/docs/components"
+            >UI Components</a
+          >
+        </p>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script>
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/vue';
 
-export default defineComponent({
+export default {
   name: 'FolderPage',
   components: {
     IonButtons,
@@ -37,9 +52,9 @@ export default defineComponent({
     IonMenuButton,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
-});
+    IonToolbar,
+  },
+};
 </script>
 
 <style scoped>
